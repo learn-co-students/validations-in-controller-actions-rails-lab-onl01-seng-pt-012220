332aec17c093b0a452085ad612c552ee2f389361
vanessuniq <vanessuniq@gmail.com>
2020-06-21 12:08:46 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      flash.now[:error] = "Unable to update this post. Please fix the following mistakes"
+      render :edit
+    end
+    
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates_presence_of :title
+    validates :category, inclusion: {in: %W(Fiction Non-Fiction), message: "%{value} is not a valid category."}
+    validates :content, length: {minimum: 100}
 end
